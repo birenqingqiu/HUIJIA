060d7110274c54e7de864a296d90c64dea45ea21
birenqingqiu <1750506511@qq.com>
2018-01-06 11:19:33 +0800

    再次修改

--- src/js/list.js
+++ src/js/list.js
@@ -7,6 +7,10 @@ require(["config"], function(){
 			// 渲染数据
 			var html = template("list_template", renderData);
 			$(".main").html(html);
+
+			// var renderData_lx = {lenovos : data.res_body.lenovo};
+			// var html_lx = template("list_template_lenovo", renderData-lx);
+			// $(".lenovo").html(html_lx);
 		});
 	});
 });
